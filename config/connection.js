9294c8ba7e9122170e466b3354cd464dd6a365e7
Nick <nick.alex.ritchie@gmail.com>
2021-09-04 23:23:32 +1000

    added node engine to package for deployment, procfile and connection jaws

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+// https://lo-victoria.com/build-a-mysql-nodejs-crud-app-4-deploying-to-heroku-finale
+// https://www.bezkoder.com/deploy-node-js-app-heroku-cleardb-mysql/#Deploy_the_app_to_Heroku
+let connection;
 
-const connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+if (process.env.JAWSDB_URL) {
+    connection = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    connection = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306,
-    }
-);
+        port: 3306
+      }
+    );
+  }
 
 module.exports = connection;
